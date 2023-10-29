93226cab68286f64b3df987c997c5f583ec6f41e
Maur1998 <mmurillosanchez1998@gmail.com>
2023-10-29 11:57:13 -0400

    configurando

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { Registro, SocialButton, Formulario, Alert } from "./components";
 
 function App() {
   return (
   <>
-    
+    <Registro />
+    <SocialButton />
+    <Formulario />
+    <Alert />
   </>
   );
 }
