69bb9381a1ec3924c2f36797c07ff41eea9939bd
XavierMurillo <xaviermurillosanchez@gmail.com>
2023-10-29 12:39:51 -0400

    created socialButton components

--- src/Components/Registro.jsx
+++ src/Components/Registro.jsx
@@ -3,7 +3,13 @@ import { SocialButton } from "./SocialButton";
 export const Registro = () => {
   return (
     <div>
-      <SocialButton />
+      <h1>Crea una cuenta</h1>
+      <div className="icon-container">
+        <SocialButton icon={"fa-brands fa-facebook fa-2xl"} />
+        <SocialButton icon={"fa-brands fa-github fa-2xl"} />
+        <SocialButton icon={"fa-brands fa-linkedin-in fa-2xl"} />
+      </div>
+      <p>O usa tu email para registrarte</p>
     </div>
   );
 };
