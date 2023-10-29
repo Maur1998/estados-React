2a82d818847e2fafca1af401da733c9377bd0a7a
Maur1998 <mmurillosanchez1998@gmail.com>
2023-10-29 11:33:52 -0400

    Creando Formulario

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -1,6 +1,24 @@
+import { Button, Form } from "react-bootstrap"
 
 export const Formulario = () => {
   return (
-    <div>Formulario</div>
+    <>
+        <Form>
+            <Form.Group className="mb-3" controlId="nombre">
+                <Form.Control type="text" placeholder="Nombre" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="email">
+                <Form.Control type="email" placeholder="tuemail@ejemplo.com" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="password">
+                <Form.Control type="password" placeholder="Contraseña" />
+            </Form.Group>
+
+
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+    </>
   )
 }
